77489e27294a037186fa87f2664a3b964213145a
mummyk <chibuzoi410@gmail.com>
2022-10-07 17:43:12 +0100

    Added responsiveness

---
+++ lib/views/mobile/mhomepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MobileHomepage extends StatelessWidget {
+  const MobileHomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blueGrey,
+    );
+  }
+}
---
+++ lib/views/web/whomepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebHomepage extends StatelessWidget {
+  const WebHomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
