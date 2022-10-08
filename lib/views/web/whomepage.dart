dfd174b9284cee2742546514a3841899d03c8dd7
mummyk <chibuzoi410@gmail.com>
2022-10-08 15:50:55 +0100

    fixing bugs

--- lib/views/web/whomepage.dart
+++ lib/views/web/whomepage.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:wears/controls/content_views.dart';
+import 'package:wears/models/custom_tab.dart';
 
-class WebHomepage extends StatelessWidget {
+class WebHomepage extends StatefulWidget {
   const WebHomepage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(26, 106, 247, 129),
-      appBar: AppBar(),
-    );
-  }
+  State<WebHomepage> createState() => _WebHomepageState();
 }
+
+class _WebHomepageState extends State<WebHomepage> {
+
+   with SingleTickerProviderStateMixin {
+  TabController tabController;
+  ItemScrollController itemScrollController;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  double screenHeight;
+  double screenWidth;
+  double topPadding;
+  double bottomPadding;
+  double sidePadding;
+
+  List<ContentView> contentViews = [
+    ContentView(
+      tab: const CustomTab(title: 'Home'),
+      content: HomeView(),
+    ),
+    ContentView(
+      tab: const CustomTab(title: 'About'),
+      content: AboutView(),
+    ),
+    ContentView(
+      tab: const CustomTab(title: 'Projects'),
+      content: ProjectsView(),
+    )
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: contentViews.length, vsync: this);
+    itemScrollController = ItemScrollController();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.end,);
+  }
+}}
+// const Scaffold(backgroundColor: Color.fromARGB(26, 106, 247, 129));
